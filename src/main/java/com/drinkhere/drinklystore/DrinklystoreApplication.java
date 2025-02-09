461a9cf4b9d804f1996bcad9a0f72e790ef131e9
이주승 <juseungl@ijuseung-ui-MacBookAir.local>
2025-02-09 20:57:24 +0900

    first commit

---
+++ src/main/java/com/drinkhere/drinklystore/DrinklystoreApplication.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinklystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinklystoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drinkhere/drinklystore/DrinklystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinklystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
