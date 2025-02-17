20a86c8965bc7d5617019aaf68dccf1dab08957c
이주승 <juseungl@ijuseung-ui-MacBookAir.local>
2025-02-17 23:08:28 +0900

    refactor: 주소 처리 방식 변경(detail address저장)

--- src/main/java/com/drinkhere/drinklystore/domain/dto/request/RegisterStoreRequest.java
+++ src/main/java/com/drinkhere/drinklystore/domain/dto/request/RegisterStoreRequest.java
@@ -7,14 +7,15 @@ public record RegisterStoreRequest(
         Long ownerId,
         String storeName,
         String storeTel,
-        String storeAddress
+        String storeAddress,
+        String storeDetailAddress
 ) {
     public Store toEntity(Coordinates coordinates) {
         return Store.builder()
                 .ownerId(ownerId)
                 .storeName(storeName)
                 .storeTel(storeTel)
-                .storeAddress(storeAddress)
+                .storeAddress(storeAddress + storeDetailAddress)
                 .latitude(coordinates.latitude())
                 .longitude(coordinates.longitude())
                 .build();
