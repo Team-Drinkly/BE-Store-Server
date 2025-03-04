b0317411a537d929680c107cc60d3cddc29a6be3
이주승 <juseungl@ijuseung-ui-MacBookAir.local>
2025-03-04 19:56:22 +0900

    refactor: 멤버쉽 사용 시 다음날 12시까지 사용 못하게 Redis 활용

--- src/main/java/com/drinkhere/drinklystore/application/service/Impl/freedrinkhistory/CreateFreeDrinkHistoryUseCase.java
+++ src/main/java/com/drinkhere/drinklystore/application/service/Impl/freedrinkhistory/CreateFreeDrinkHistoryUseCase.java
@@ -8,8 +8,16 @@ import com.drinkhere.drinklystore.domain.service.freedrinkhistory.FreeDrinkHisto
 import com.drinkhere.drinklystore.domain.service.store.StoreQueryService;
 import com.drinkhere.drinklystore.openfeign.client.MemberClient;
 import com.drinkhere.drinklystore.openfeign.dto.response.MemberResponse;
+import com.drinkhere.drinklystore.redis.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+
+import static com.drinkhere.drinklystore.common.exception.store.StoreErrorCode.ALREADY_USED_TODAY;
 import static com.drinkhere.drinklystore.common.exception.store.StoreErrorCode.NOT_SUBSCIRBER;
 
 @ApplicationService
@@ -18,16 +26,40 @@ public class CreateFreeDrinkHistoryUseCase {
     private final FreeDrinkHistoryCommandService freeDrinkHistoryCommandService;
     private final StoreQueryService storeQueryService;
     private final MemberClient memberClient;
+    private final RedisUtil redisUtil;
+    private static final String REDIS_KEY_PREFIX = "free-drink:";
+
 
     public void createFreeDrinkHistory(String isSubscribe, Long memberId, Long subscribeId, CreateFreeDrinkHistoryRequest createFreeDrinkHistoryRequest) {
         if (isSubscribe.equals("false")) throw new StoreException(NOT_SUBSCIRBER);
         Store store = storeQueryService.findById(createFreeDrinkHistoryRequest.storeId());
 
+        String redisKey = REDIS_KEY_PREFIX + memberId;
+        if (redisUtil.hasKey(redisKey)) {
+            throw new StoreException(ALREADY_USED_TODAY); // 오늘 이미 사용한 경우 예외 발생
+        }
         // Feign Client 서버 간 통신으로 member nickname 들고옴.
         MemberResponse memberResponse = memberClient.getMemberById(memberId);
         String memberNickname = memberResponse.getNickname();
 
         freeDrinkHistoryCommandService.createFreeDrinkHistory(memberId, memberNickname, subscribeId, store, createFreeDrinkHistoryRequest.providedDrink());
+
+        // 현재 시간
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+
+        // 오늘 오후 12:00 (정오) 시간
+        LocalDateTime todayNoon = LocalDate.now().atTime(12, 0);
+
+        // 만료 시간 설정 (현재 시간이 오후 12:00 이후라면 내일 정오)
+        LocalDateTime expirationTime = now.isAfter(todayNoon)
+                ? todayNoon.plusDays(1)  // 내일 오후 12:00
+                : todayNoon;              // 오늘 오후 12:00
+
+        // TTL 계산
+        long ttl = Duration.between(now, expirationTime).getSeconds();
+
+        // Redis에 저장
+        redisUtil.saveAsValue(redisKey, "USED", ttl, TimeUnit.SECONDS);
     }
 
 }
---
+++ src/main/java/com/drinkhere/drinklystore/redis/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.drinkhere.drinklystore.redis.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisConfig {
+    private String host;
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager oidcCacheManager(RedisConnectionFactory cf) {
+        RedisCacheConfiguration redisCacheConfiguration =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeKeysWith(
+                                RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new StringRedisSerializer()))
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new GenericJackson2JsonRedisSerializer()))
+                        .entryTtl(Duration.ofDays(1L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
