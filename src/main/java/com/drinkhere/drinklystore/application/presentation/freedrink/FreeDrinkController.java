25a6df41af5f44794734887971bdb7cec1f499b9
이주승 <juseungl@ijuseung-ui-MacBookAir.local>
2025-04-25 11:22:40 +0900

    refactor: 멤버쉽 당일 여러 가게 사용 가능토록 변경

--- src/main/java/com/drinkhere/drinklystore/application/presentation/freedrink/FreeDrinkController.java
+++ src/main/java/com/drinkhere/drinklystore/application/presentation/freedrink/FreeDrinkController.java
@@ -27,7 +27,8 @@ public class FreeDrinkController implements FreeDrinkControllerDocs {
             @RequestHeader(value = "is-subscribed", required = false) String isSubscribe,
             @RequestBody CreateFreeDrinkHistoryRequest createFreeDrinkHistoryRequest
     ) {
-        createFreeDrinkHistoryUseCase.createFreeDrinkHistory(isSubscribe, memberId, subscribeId, createFreeDrinkHistoryRequest);
+
+        createFreeDrinkHistoryUseCase.createFreeDrinkHistoryV2(isSubscribe, memberId, subscribeId, createFreeDrinkHistoryRequest);
         return ApplicationResponse.created("Free Drink History를 성공적으로 생성했습니다.");
     }
 
